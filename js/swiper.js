1e40cabc4833978207e304182ab5d2f3a4c8f148
dinnerdaichi <yamaguchi.daichandayo@gmail.com>
2022-04-20 20:21:42 +0900

    new

---
+++ js/swiper.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper(".swiper", {
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+
+  	pagination: {
+  	  el: '.swiper-pagination',
+  	  type: 'bullets',
+  	  clickable: true
+  	},
+    loop:true,
+    slidesPerView: 1.5,
+    spaceBetween: 20,
+    centeredSlides: true,
+    speed:800,
+    
+
+    	// autoplay: {
+    	//   // スライドが動く間隔。(これなら3秒)
+    	//   delay: 3000,
+    	//   // trueなら最後のスライドまで行ったら停止する。falseなら最初に戻る
+    	//   stopOnLastSlide: false,
+    	//   // trueなら触った後停止falseなら再生され続ける
+    	//   disableOnInteraction: false,
+    	//   // trueなら最後のスライドまで行ったら停止する。falseなら最初に戻る
+    	//   reverseDirection: false
+    	// },
+});
