4c7fe093565e61bc3c2e4943b84a9e022592d35d
dinnerdaichi <yamaguchi.daichandayo@gmail.com>
2022-04-22 00:16:16 +0900

    wow2

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@ $(function () {
 
   
 
-  $(".main_contents").click(function () {
-    $(".sp_header").toggleClass('active');
-    $(".sp_back").toggleClass('active');
+  $(".sp_btn").click(function () {
+    $(".sp_header").addClass('active');
+    $(".sp_back").addClass('active');
   });
+
+  $(".nav_items").click(function () {
+    $(".sp_header").removeClass('active');
+    $(".sp_back").removeClass('active');
+  });
+
+  
 
 
   $(document).ready(function () {
