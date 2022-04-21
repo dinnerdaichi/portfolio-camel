90e9a0526c44ce3e14757cba662fe3fcd335130a
dinnerdaichi <yamaguchi.daichandayo@gmail.com>
2022-04-22 01:31:24 +0900

    一旦

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
 $(function () {
 
-  
+
 
   $(".sp_btn").click(function () {
+
+    $(".sp_btn").addClass('active');
     $(".sp_header").addClass('active');
     $(".sp_back").addClass('active');
   });
 
   $(".nav_items").click(function () {
+    $(".sp_btn").removeClass('active');
     $(".sp_header").removeClass('active');
     $(".sp_back").removeClass('active');
   });
 
-  
+
 
 
   $(document).ready(function () {
-    $(".loading").addClass("hide"); 
+    $(".loading").addClass("hide");
   });
 
 });
