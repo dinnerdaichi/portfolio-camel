7a4838674190d4c19befd79ee7307a9727762e9b
dinnerdaichi <yamaguchi.daichandayo@gmail.com>
2024-07-23 18:15:13 +0900

    [update]スキル部分更新

--- js/script.js
+++ js/script.js
@@ -91,11 +91,11 @@ $(document).click(function (event) {
 
 $(function () {
   setTimeout(function () {
-    $('.logo_fadein p').fadeIn(500);
+    $('.logo_fadein p').fadeIn(1000);
   }, 500);
   setTimeout(function () {
     $('.logo_fadein').fadeOut(500);
-  }, 2500);
+  }, 2000);
 });
 
 
